e7b3b22b7c8a343ade5c214aff88532095844cfe
Matthew Nerger <mnerger@lsu.edu>
2016-06-26 15:40:54 -0400

    Remove sample links and tooltips to bandcamp

--- framework/bootstrap/js/bootstrap.js
+++ framework/bootstrap/js/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=a8fc212da511d8bc71e5c8b44f2e6542)
- * Config saved to config.json and https://gist.github.com/a8fc212da511d8bc71e5c8b44f2e6542
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=ab3cf93f9744990387a38b512c585054)
+ * Config saved to config.json and https://gist.github.com/ab3cf93f9744990387a38b512c585054
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
