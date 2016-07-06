6cbfbb836bd360a0e25b47fc3bc01c928ca58f54
Matthew Nerger <mnerger@lsu.edu>
2016-07-06 14:03:31 -0400

    Fix audio urls

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,6 @@ $('#fsu-img').click(function() {
 //code based on tutorial at https://www.script-tutorials.com/html5-audio-player-with-playlist
 
 var song;
-song.volume = 1;
 
 //playlist object
 var playlist = {
@@ -28,82 +27,82 @@ var playlist = {
       {
         name: 'der Mond',
         el: $('#mond-01'),
-        url: '../audio/der-mond/01.ogg'
+        url: 'audio/der-mond/01.ogg'
       },
       {
         name: 'die Sonne',
         el: $('#mond-02'),
-        url: '../audio/der-mond/02.ogg'
+        url: 'audio/der-mond/02.ogg'
       },
       {
         name: 'der Himmel',
         el: $('#mond-03'),
-        url: '../audio/der-mond/03.ogg'
+        url: 'audio/der-mond/03.ogg'
       },
       {
         name: 'die Sternen',
         el: $('#mond-04'),
-        url: '../audio/der-mond/04.ogg'
+        url: 'audio/der-mond/04.ogg'
       },
       {
         name: 'for Cardboard and Digital Oceans',
         el: $('#mond-05'),
-        url: '../audio/der-mond/05.ogg'
+        url: 'audio/der-mond/05.ogg'
       },
       {
         name: 'for Piano and Locomotive',
         el: $('#mond-06'),
-        url: '../audio/der-mond/06.ogg'
+        url: 'audio/der-mond/06.ogg'
       },
       {
         name: "'I don't wanna die'",
         el: $('#mond-07'),
-        url: '../audio/der-mond/07.ogg'
+        url: 'audio/der-mond/07.ogg'
       },
       {
         name: 'for Bell Tower and Reverb Pedal',
         el: $('#mond-08'),
-        url: '../audio/der-mond/08.ogg'
+        url: 'audio/der-mond/08.ogg'
       },
       {
         name: "'The Air After the Flood'",
         el: $('#mond-09'),
-        url: '../audio/der-mond/09.ogg'
+        url: 'audio/der-mond/09.ogg'
       },
       {
         name: 'for Clarinet and Night Sky',
         el: $('#mond-10'),
-        url: '../audio/der-mond/10.ogg'
+        url: 'audio/der-mond/10.ogg'
       },
       {
         name: 'for Organs and Drainpipe',
         el: $('#mond-11'),
-        url: '../audio/der-mond/11.ogg'
+        url: 'audio/der-mond/11.ogg'
       },
       {
         name: 'Ghost',
         el: $('#mond-12'),
-        url: '../audio/der-mond/12.ogg'
+        url: 'audio/der-mond/12.ogg'
       },
       {
         name: 'der Mond',
         el: $('#mond-13'),
-        url: '../audio/der-mond/13.ogg'
+        url: 'audio/der-mond/13.ogg'
       },
       {
         name: 'Thaw',
         el: $('#mond-14'),
-        url: '../audio/der-mond/14.ogg'
+        url: 'audio/der-mond/14.ogg'
       },
       {
         name: 'the Road to Tallahassee, pt. II',
         el: $('#mond-15'),
-        url: '../audio/der-mond/15.ogg'
+        url: 'audio/der-mond/15.ogg'
       },
       {
         name: 'Crocodile/the Moon',
         el: $('#mond-16'),
-        url: '../audio/der-mond/16.ogg'
+        url: 'audio/der-mond/16.ogg'
       }
     ]
   },
@@ -113,12 +112,12 @@ var playlist = {
       {
         name: 'vol. 1',
         el: $('#pop-01'),
-        url: '../audio/popular-music/01.ogg'
+        url: 'audio/popular-music/01.ogg'
       },
       {
         name: 'vol. 2',
         el: $('#pop-02'),
-        url: '../audio/popular-music/02.ogg'
+        url: 'audio/popular-music/02.ogg'
       }
     ]
   },
