e2fc43a4e7d00ffab110852b56a3fffc357f2a9b
Matthew Nerger <mnerger@lsu.edu>
2016-07-06 13:19:34 -0400

    Construct music player framework

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//code to open modules
+
 $('#der-mond-img').click(function() {
   $('#der-mond-modal').modal();
 });
@@ -9,3 +11,33 @@ $('#popular-music-img').click(function() {
 $('#fsu-img').click(function() {
   $('#fsu-modal').modal();
 });
+
+
+
+//----------------------MUSIC PLAYER-------------------
+//code based on tutorial at https://www.script-tutorials.com/html5-audio-player-with-playlist
+
+var song;
+song.volume = 1;
+
+//playlist object
+var playlist = {
+  derMond: {
+    title: "der Mond (ii)",
+    tracks: {
+
+    }
+  },
+  popMusic: {
+    title: "popular music, vols. 1 & 2",
+    tracks: {
+
+    }
+  },
+  fsu: {
+    title: "florida state universiry 2011-2013",
+    tracks: {
+
+    }
+  }
+};
