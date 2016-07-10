98802c698f6ef6e94d5c9143003e98fe47152ee7
Matthew Nerger <mnerger@lsu.edu>
2016-07-10 19:31:56 -0400

    Create functional tabs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 (function() {
   var app = angular.module('portfolio', ['album-page']);
+
+  app.controller('PageSwitcher', function() {
+    this.tab = 0;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
 })();
