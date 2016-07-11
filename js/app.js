804684660aebac38781c4782f4b88df41bf65f68
Matthew Nerger <mnerger@lsu.edu>
2016-07-10 20:34:09 -0400

    Move audio bar

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
   var app = angular.module('portfolio', ['album-page']);
 
   app.controller('PageSwitcher', function() {
-    this.tab = -1;
+    this.tab = 0;
 
     this.selectTab = function(setTab) {
       this.tab = setTab;
